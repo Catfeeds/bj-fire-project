fefe1cf16fa7fb0d4bac7c60b0b0cf6e82db1db2
wangcailin <dreamcailin@hotmail.com>
2017-12-21 18:25:08 +0800

    归档

--- public/assets/js/backend/category.js
+++ public/assets/js/backend/category.js
@@ -1,4 +1,4 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'template'], function ($, undefined, Backend, Table, Form, Template) {
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
 
     var Controller = {
         index: function () {
@@ -16,48 +16,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'template'], function
 
             var table = $("#table");
 
-            //在普通搜索提交搜索前
-            table.on('common-search.bs.table', function (event, table, params, query) {
-                //这里可以对params值进行修改,从而影响搜索条件
-                params.filter = JSON.parse(params.filter);
-                params.op = JSON.parse(params.op);
-
-                // 判断联动选择项是否选择，进行有增/删
-                if($.trim($("select[name='pid1']").val())!=''){
-                    params.filter['pid'] = $.trim($("select[name='pid1']").val());
-                    params.op['pid'] = "=";
-                }
-                if($.trim($("select[name='pid2']").val())!=''){
-                    params.filter['pid'] = $.trim($("select[name='pid2']").val());
-                    params.op['pid'] = "=";
-                }
-                if($.trim($("select[name='pid3']").val())!=''){
-                    params.filter['pid'] = $.trim($("select[name='pid3']").val());
-                    params.op['pid'] = "=";
-                }
-                if($.trim($("select[name='pid4']").val())!=''){
-                    params.filter['pid'] = $.trim($("select[name='pid4']").val());
-                    params.op['pid'] = "=";
-                }
-
-                params.filter = JSON.stringify(params.filter);
-                params.op = JSON.stringify(params.op);
-
-                return params;
-            });
-
-            //在普通搜索渲染后
-            table.on('post-common-search.bs.table', function (event, table) {
-                $("input[name='title']").addClass("selectpage").data("source", "auth/adminlog/selectpage").data("primaryKey", "title").data("field", "title").data("orderBy", "id desc");
-                Form.events.cxselect($("form", table.$commonsearch));
-                Form.events.selectpage($("form", table.$commonsearch));
-            });
-
-            //在表格内容渲染完成后回调的事件
-            table.on('post-body.bs.table', function (e, settings, json, xhr) {
-
-            });
-
             // 初始化表格
             table.bootstrapTable({
                 url: $.fn.bootstrapTable.defaults.extend.index_url,
@@ -67,23 +25,17 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'template'], function
                 columns: [
                     [
                         {checkbox: true},
-                        {field: 'id', title: __('Id'), operate: false},
-                        {field: 'pid', title: __('Pid'), searchList: function () {
-                            return Template('categorytpl', {});
-                        }},
-                        {field: 'type_text', title: __('Type'), operate: false},
-                        {field: 'name', title: __('Name'), align: 'left', operate: false},
-                        {field: 'accounted', title: __('Accounted'), operate: false},
-                        {field: 'status', title: __('Status'), operate: false, formatter: Table.api.formatter.status},
-                        {field: 'weigh', title: __('Weigh'), operate: false},
+                        {field: 'id', title: __('Id')},
+                        {field: 'pid', title: __('Pid')},
+                        {field: 'type_text', title: __('Type')},
+                        {field: 'name', title: __('Name'), align: 'left'},
+                        {field: 'accounted', title: __('Accounted')},
+                        {field: 'status', title: __('Status'), formatter: Table.api.formatter.status},
+                        {field: 'weigh', title: __('Weigh')},
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ],
-                queryParams: function (params) {
-                    params.filter = JSON.stringify(params.filter);
-                    params.op = JSON.stringify(params.op);
-                    return params;
-                },
+                commonSearch: false
             });
 
             // 为表格绑定事件
